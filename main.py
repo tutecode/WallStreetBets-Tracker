c4eb4fcb238add11c145476a66f2b59d4bdf98fe
TuteCode <54072234+tutecode@users.noreply.github.com>
2021-08-25 18:39:44 -0300

    update main.py

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ api = PushshiftAPI()
 # First 10 submissions to /r/wallstreetbets in 2021, filtering results to url/author/title/subreddit fields.
 start_epoch = int(dt.datetime(2021, 8, 15).timestamp())
 
-submissions = list(api.search_submissions(after=start_epoch,
-                                          subreddit='wallstreetbets',
-                                          filter=['url', 'author',
-                                                  'title', 'subreddit'],
-                                          limit=10))
+# Search to the end
+submissions = api.search_submissions(after=start_epoch,
+                                     subreddit='wallstreetbets',
+                                     filter=['url', 'author',
+                                             'title', 'subreddit'])
 
 for submission in submissions:
-    print(submission)
+    # print(submission)
+    print(submission.created_utc)
+    print(submission.title)
+    print(submission.url)
+
+    words = submission.title.split()
+    print(words)
